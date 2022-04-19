112cc095203467b587d1e2537f193d4df37131ac
Akifumi Maeda <akifumi_maeda@maede.co.jp>
2022-04-19 03:09:34 +0000

    fix some

--- config/settings.py
+++ config/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join('BASE_DIR', 'templates')], # テンプレートファイルの検索ディレクトリを指定
+        'DIRS': [os.path.join(BASE_DIR, 'templates')], # テンプレートファイルの検索ディレクトリを指定
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
